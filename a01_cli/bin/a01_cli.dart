c936372f5b8e1ff9b83bd8654e5e1c80ac46a1ee
yusuke mizushima <mzsima@gmail.com>
2021-12-26 00:15:09 +0900

    hello dart

---
+++ a01_cli/bin/a01_cli.dart
@@ -0,0 +1,5 @@
+import 'package:a01_cli/a01_cli.dart' as a01_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${a01_cli.calculate()}!');
+}
---
+++ a01_cli/test/a01_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:a01_cli/a01_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
