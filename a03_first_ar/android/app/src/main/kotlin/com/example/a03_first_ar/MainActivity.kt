0ccf83f34d3ba48cf7f8bcf1ed3e49d97ebcdc22
yusuke mizushima <mzsima@gmail.com>
2022-01-07 23:06:51 +0900

    hello ar kit

---
+++ a03_first_ar/android/app/src/main/kotlin/com/example/a03_first_ar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a03_first_ar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ a03_first_ar/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:vector_math/vector_math_64.dart';
+
+void main() => runApp(MaterialApp(home: MyApp()));
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late ARKitController arkitController;
+
+  @override
+  void dispose() {
+    arkitController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(title: const Text('ARKit in Flutter')),
+      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+    final node = ARKitNode(
+        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, 0));
+    this.arkitController.add(node);
+  }
+}
